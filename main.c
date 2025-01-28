877a8affd1aaa4ffd073945e4c53ef05ca4a989d
unknown <ezekuiel100@gmail.com>
2025-01-28 10:40:33 -0300

    select operator

--- main.c
+++ main.c
@@ -1,16 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     int num, num2;
+    char operator;
 
     printf("Digite um numero: ");
-    scanf("%d", &num);
+    scanf(" %d", &num);
+
+    printf("Operacao: ");
+    scanf(" %c", &operator);
 
     printf("Digite outro numero: ");
-    scanf("%d", &num2);
+    scanf(" %d", &num2);
 
-    printf("%d", num + num2);
+    switch (operator)
+    {
+    case '+':
+        printf("Resultado: %d\n", num + num2);
+        break;
+    case '-':
+        printf("Resultado: %d\n", num - num2);
+        break;
+
+    case '*':
+        printf("Resultado: %d\n", num * num2);
+        break;
+
+    case '/':
+        printf("Resultado: %d\n", num / num2);
+        break;
+
+    default:
+        printf("Operação inválida!\n");
+        break;
+    }
 
     return 0;
 }
